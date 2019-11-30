d36078d2ed85c6b6d1e0ea23faa441ab88d9139b
miao <rajor37@gmail.com>
2019-11-30 17:14:38 -0500

    warning

---
+++ Main.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String filename = args[0];
+
+        try {
+            List list = readByJavaClassic(filename);
+            list.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static List readByJava8(String fileName) throws IOException {
+        List<String> result;
+        try (Stream<String> lines = Files.lines(Paths.get(fileName))) {
+            result = lines.collect(Collectors.toList());
+        }
+        return result;
+
+    }
+
+    private static List readByJava7(String fileName) throws IOException {
+        return Files.readAllLines(new File(fileName).toPath(), Charset.defaultCharset());
+    }
+
+    private static List readByJavaClassic(String fileName) throws IOException {
+
+        List<String> result = new ArrayList<>();
+        BufferedReader br = null;
+
+        try {
+
+            br = new BufferedReader(new FileReader(fileName));
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                result.add(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                br.close();
+            }
+        }
+
+        return result;
+    }
+
+}
